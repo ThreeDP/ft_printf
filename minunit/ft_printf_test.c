eba5772db1c4600748f5a168209950ebfffc09af
Davy Paulino <dapaulin@c1r6p4.42sp.org.br>
2022-11-09 21:39:12 -0300

    Add the tests from de ft_printf

--- minunit/ft_printf_test.c
+++ minunit/ft_printf_test.c
@@ -1,10 +1,12 @@
 #include "minunit.h"
-#include "get_next_line.h"
+#include "../srcs/ft_printf.h"
+#include "./get_next_line.h"
+#include "ft_printf_test.h"
 
 MU_TEST_SUITE(passing_a_char_D_print_in_the_terminal)
 {
     //ARRANGE
-    char    *file = "./file"
+    char    *file = "./files/D";
     char    *expected_result = "D";
     int     expected_bytes = 1;
     char    *result_str;
@@ -14,7 +16,31 @@ MU_TEST_SUITE(passing_a_char_D_print_in_the_terminal)
         return ;
     
     //ACT
-    bytes = ft_printf("D");
+    bytes = ft_printf(fd, "D");
+    close(fd);
+    fd = open(file, O_RDONLY);
+    result_str = get_next_line(fd);
+    close(fd);
+
+    //ASSERT
+    mu_assert_int_eq(expected_bytes, bytes);
+    mu_assert_string_eq(expected_result, result_str);
+}
+
+MU_TEST_SUITE(passing_a_text_of_lord_of_rings_printf_the_text_in_the_terminal)
+{
+    //ARRANGE
+    char    *file = "./files/One-ring";
+    char    *expected_result = "One Ring to rule them all, One Ring to find them, One Ring to bring them all, and in the darkness bind them, In the Land of Mordor where the Shadows lie.";
+    int     expected_bytes = 153;
+    char    *result_str;
+    int     bytes;
+    int     fd = open(file, O_WRONLY);
+    if (!fd)
+        return ;
+    
+    //ACT
+    bytes = ft_printf(fd, "One Ring to rule them all, One Ring to find them, One Ring to bring them all, and in the darkness bind them, In the Land of Mordor where the Shadows lie.");
     close(fd);
     fd = open(file, O_RDONLY);
     result_str = get_next_line(fd);
@@ -28,10 +54,11 @@ MU_TEST_SUITE(passing_a_char_D_print_in_the_terminal)
 MU_TEST_SUITE(test_suite)
 {	
     MU_RUN_TEST(passing_a_char_D_print_in_the_terminal);
+    MU_RUN_TEST(passing_a_text_of_lord_of_rings_printf_the_text_in_the_terminal);
 }
 
 int main() {
-	MU_RUN_SUITE(passing_a_char_D_print_in_the_terminal);
+	MU_RUN_SUITE(test_suite);
 	MU_REPORT();
 	return MU_EXIT_CODE;
 }
---
+++ minunit/ft_printf_test.h
@@ -0,0 +1,9 @@
+#ifndef FT_PRINTF_TEST_H
+# define FT_PRINTF_TEST_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+#endif
