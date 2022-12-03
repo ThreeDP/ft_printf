a2d431c0d62a91e608bfd5f86b081e01e141f9ee
Davy Paulino <dapaulin@c3r5p1.42sp.org.br>
2022-12-03 19:06:43 -0300

    Refactoring Test: Create functions for setup and unset the pattern config for the tests

---
+++ minunit/ft_printf_bonus_test.c
@@ -0,0 +1,61 @@
+#include "minunit.h"
+#include "../srcs/ft_printf.h"
+#include "./get_next_line.h"
+#include "ft_printf_test.h"
+
+int setup(char *file, int *fd)
+{
+    int     bkp;
+    
+    *fd = open(file, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+    if (!*fd)
+        return (0);
+    bkp = dup(1);
+    dup2(*fd, 1);
+    return (bkp);
+}
+
+char *unset(int fd, char *file, int *bkp)
+{
+    char *text;
+
+    close(fd);
+    fd = open(file, O_RDONLY);
+    text = get_next_line(fd);
+    dup2(*bkp, 1);
+    remove(file);
+    return (text);
+}
+
+MU_TEST_SUITE(passing_a_5_flag_for_the_string_davy_and_return_space_davy)
+{
+    //ARRANGE
+    int     fd;
+    int     bytes;
+    char    *result_str;
+    char    *file               = "./files/initial-hashtag";
+    char    *word               = "Davy";
+    int     bkp                 = setup(file, &fd);
+    char    *expected_result    = "The first name of he is Davy!";
+    int     expected_bytes      = 29;
+    
+    //ACT
+    bytes = ft_printf("The first name of he is%5s!", word);
+
+    //ASSERT
+    result_str = unset(fd, file, &bkp);
+    mu_assert_string_eq(expected_result, result_str);
+    mu_assert_int_eq(expected_bytes, bytes);
+    free(result_str);
+}
+
+MU_TEST_SUITE(test_suite)
+{	
+    MU_RUN_TEST(passing_a_5_flag_for_the_string_davy_and_return_space_davy);
+}
+
+int main() {
+	MU_RUN_SUITE(test_suite);
+	MU_REPORT();
+	return MU_EXIT_CODE;
+}
--- minunit/printstring_test.c
+++ minunit/printstring_test.c
@@ -3,422 +3,326 @@
 #include "./get_next_line.h"
 #include "ft_printf_test.h"
 
+t_format *setup(char *file, int  *fd)
+{
+    t_format    *shape;
+
+    *fd = open(file, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+    if (!*fd)
+        return (NULL);
+    shape              = (t_format *) malloc(sizeof(t_format));
+    if (!shape)
+        return (NULL);
+    return (shape);
+}
+
+char *unset(int fd, char *file)
+{
+    char *text;
+
+    close(fd);
+    fd = open(file, O_RDONLY);
+    text = get_next_line(fd);
+    remove(file);
+    return (text);
+}
+
+void    free_shape(t_format **shape, char *str)
+{
+    if (str)
+        free(str);
+    if ((*shape)->arg)
+        free((*shape)->arg);
+    if ((*shape)->flags)
+        free((*shape)->flags);
+    if ((*shape))
+        free((*shape));
+}
+
 MU_TEST_SUITE(passing_a_11_flag_for_the_string_One_Ring_and_return_three_spaces_One_Ring)
 {
     //CONFIG
+    int             fd;
     char            *file               = "./files/string/one-ring";
-    int             fd = open(file, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
-    if (!fd)
-        return ;
-    t_format        *shape              = (t_format *) malloc(sizeof(t_format));
-    if (!shape)
-        return ;
+    t_format        *shape              = setup(file, &fd);
+
     //ARRANGE
-    shape->type                         = 's';
-    shape->flags                        = ft_strdup("11");
+    int             bsr;
+    char            *result_str;
     char            *str                = "One Ring";
     char            *expected_result    = "   One Ring";
-    int             bsr;
     int             expected_bsr        = 11;
+    shape->type                         = 's';
+    shape->flags                        = ft_strdup("11");
 
     //ACT
     bsr = printstring(fd, str, &shape);
-    close(fd);
-    fd = open(file, O_RDONLY);
-    str = get_next_line(fd);
-    remove(file);
+    result_str = unset(fd, file);
 
     //ASSERTS
-    mu_assert_string_eq(expected_result, str);
+    mu_assert_string_eq(expected_result, result_str);
     mu_assert_int_eq(expected_bsr, bsr);
-    if (shape->arg)
-        free(shape->arg);
-    if (shape->flags)
-        free(shape->flags);
-    if (shape)
-        free(shape);
-    if (str)
-        free(str);
+    free_shape(&shape, result_str);
 }
 
 MU_TEST_SUITE(passing_a_minus_11_flag_for_the_string_One_Ring_and_return_One_Ring_three_spaces)
 {
     //CONFIG
+    int             fd;
     char            *file               = "./files/string/one-ring-reverse";
-    int             fd = open(file, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
-    if (!fd)
-        return ;
-    t_format        *shape              = (t_format *) malloc(sizeof(t_format));
-    if (!shape)
-        return ;
+    t_format        *shape              = setup(file, &fd);
+
     //ARRANGE
-    shape->type                         = 's';
-    shape->flags                        = ft_strdup("-11");
+    int             bsr;
+    char            *result_str;
     char            *str                = "One Ring";
     char            *expected_result    = "One Ring   ";
-    int             bsr;
     int             expected_bsr        = 11;
+    shape->type                         = 's';
+    shape->flags                        = ft_strdup("-11");
 
     //ACT
     bsr = printstring(fd, str, &shape);
-    close(fd);
-    fd = open(file, O_RDONLY);
-    str = get_next_line(fd);
-    remove(file);
+    result_str = unset(fd, file);
 
     //ASSERTS
-    mu_assert_string_eq(expected_result, str);
+    mu_assert_string_eq(expected_result, result_str);
     mu_assert_int_eq(expected_bsr, bsr);
-    if (shape->arg)
-        free(shape->arg);
-    if (shape->flags)
-        free(shape->flags);
-    if (shape)
-        free(shape);
-    if (str)
-        free(str);
+    free_shape(&shape, result_str);
 }
 
 MU_TEST_SUITE(passing_a_flag_11_dot_3_and_a_string_One_Ring_should_be_8_spaces_One)
 {
     //CONFIG
+    int             fd;
     char            *file               = "./files/string/one";
-    int             fd = open(file, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
-    if (!fd)
-        return ;
-    t_format        *shape              = (t_format *) malloc(sizeof(t_format));
-    if (!shape)
-        return ;
+    t_format        *shape              = setup(file, &fd);
+
     //ARRANGE
-    shape->type                         = 's';
-    shape->flags                        = ft_strdup("11.3");
+    int             bsr;
+    char            *result_str;
     char            *str                = "One Ring";
     char            *expected_result    = "        One";
-    int             bsr;
     int             expected_bsr        = 11;
+    shape->type                         = 's';
+    shape->flags                        = ft_strdup("11.3");
 
     //ACT
     bsr = printstring(fd, str, &shape);
-    close(fd);
-    fd = open(file, O_RDONLY);
-    str = get_next_line(fd);
-    remove(file);
+    result_str = unset(fd, file);
 
     //ASSERTS
-    mu_assert_string_eq(expected_result, str);
+    mu_assert_string_eq(expected_result, result_str);
     mu_assert_int_eq(expected_bsr, bsr);
-    if (shape->arg)
-        free(shape->arg);
-    if (shape->flags)
-        free(shape->flags);
-    if (shape)
-        free(shape);
-    if (str)
-        free(str);
+    free_shape(&shape, result_str);
 }
 
 MU_TEST_SUITE(passing_a_flag_4_dot_5_and_a_string_One_Ring_should_be_One_R)
 {
     //CONFIG
+    int             fd;
     char            *file               = "./files/string/4dot5";
-    int             fd = open(file, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
-    if (!fd)
-        return ;
-    t_format        *shape              = (t_format *) malloc(sizeof(t_format));
-    if (!shape)
-        return ;
+    t_format        *shape              = setup(file, &fd);
+
     //ARRANGE
-    shape->type                         = 's';
-    shape->flags                        = ft_strdup("4.5");
+    int             bsr;
+    char            *result_str;
     char            *str                = "One Ring";
     char            *expected_result    = "One R";
-    int             bsr;
     int             expected_bsr        = 5;
+    shape->type                         = 's';
+    shape->flags                        = ft_strdup("4.5");
 
     //ACT
     bsr = printstring(fd, str, &shape);
-    close(fd);
-    fd = open(file, O_RDONLY);
-    str = get_next_line(fd);
-    remove(file);
+    result_str = unset(fd, file);
 
     //ASSERTS
-    mu_assert_string_eq(expected_result, str);
+    mu_assert_string_eq(expected_result, result_str);
     mu_assert_int_eq(expected_bsr, bsr);
-    if (shape->arg)
-        free(shape->arg);
-    if (shape->flags)
-        free(shape->flags);
-    if (shape)
-        free(shape);
-    if (str)
-        free(str);
+    free_shape(&shape, result_str);
 }
 
 MU_TEST_SUITE(passing_a_flag_5_dot_2_and_a_string_One_Ring_should_be_On)
 {
     //CONFIG
+    int             fd;
     char            *file               = "./files/string/5dot2";
-    int             fd = open(file, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
-    if (!fd)
-        return ;
-    t_format        *shape              = (t_format *) malloc(sizeof(t_format));
-    if (!shape)
-        return ;
+    t_format        *shape              = setup(file, &fd);
+
     //ARRANGE
-    shape->type                         = 's';
-    shape->flags                        = ft_strdup("5.2");
+    int             bsr;
+    char            *result_str;
     char            *str                = "One Ring";
     char            *expected_result    = "   On";
-    int             bsr;
     int             expected_bsr        = 5;
+    shape->type                         = 's';
+    shape->flags                        = ft_strdup("5.2");
 
     //ACT
     bsr = printstring(fd, str, &shape);
-    close(fd);
-    fd = open(file, O_RDONLY);
-    str = get_next_line(fd);
-    remove(file);
+    result_str = unset(fd, file);
 
     //ASSERTS
-    mu_assert_string_eq(expected_result, str);
+    mu_assert_string_eq(expected_result, result_str);
     mu_assert_int_eq(expected_bsr, bsr);
-    if (shape->arg)
-        free(shape->arg);
-    if (shape->flags)
-        free(shape->flags);
-    if (shape)
-        free(shape);
-    if (str)
-        free(str);
+    free_shape(&shape, result_str);
 }
 
 MU_TEST_SUITE(passing_a_flag_3_dot_0_and_a_string_One_Ring_should_be_3_spaces)
 {
     //CONFIG
+    int             fd;
     char            *file               = "./files/string/3dot0";
-    int             fd = open(file, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
-    if (!fd)
-        return ;
-    t_format        *shape              = (t_format *) malloc(sizeof(t_format));
-    if (!shape)
-        return ;
+    t_format        *shape              = setup(file, &fd);
+
     //ARRANGE
-    shape->type                         = 's';
-    shape->flags                        = ft_strdup("3.0");
+    int             bsr;
+    char            *result_str;
     char            *str                = "One Ring";
     char            *expected_result    = "   ";
-    int             bsr;
     int             expected_bsr        = 3;
+    shape->type                         = 's';
+    shape->flags                        = ft_strdup("3.0");
 
     //ACT
     bsr = printstring(fd, str, &shape);
-    close(fd);
-    fd = open(file, O_RDONLY);
-    str = get_next_line(fd);
-    remove(file);
+    result_str = unset(fd, file);
 
     //ASSERTS
-    mu_assert_string_eq(expected_result, str);
+    mu_assert_string_eq(expected_result, result_str);
     mu_assert_int_eq(expected_bsr, bsr);
-    if (shape->arg)
-        free(shape->arg);
-    if (shape->flags)
-        free(shape->flags);
-    if (shape)
-        free(shape);
-    if (str)
-        free(str);
+    free_shape(&shape, result_str);
 }
 
 MU_TEST_SUITE(passing_a_flag_minus_30_dot_20_and_a_string_One_Ring_should_be_3_spaces)
 {
     //CONFIG
+    int             fd;
     char            *file               = "./files/string/minus30dot20";
-    int             fd = open(file, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
-    if (!fd)
-        return ;
-    t_format        *shape              = (t_format *) malloc(sizeof(t_format));
-    if (!shape)
-        return ;
+    t_format        *shape              = setup(file, &fd);
+
     //ARRANGE
-    shape->type                         = 's';
-    shape->flags                        = ft_strdup("-30.20");
+    int             bsr;
+    char            *result_str;
     char            *str                = "One Ring";
     char            *expected_result    = "One Ring                      ";
-    int             bsr;
     int             expected_bsr        = 30;
+    shape->type                         = 's';
+    shape->flags                        = ft_strdup("-30.20");
 
     //ACT
     bsr = printstring(fd, str, &shape);
-    close(fd);
-    fd = open(file, O_RDONLY);
-    str = get_next_line(fd);
-    remove(file);
+    result_str = unset(fd, file);
 
     //ASSERTS
-    mu_assert_string_eq(expected_result, str);
+    mu_assert_string_eq(expected_result, result_str);
     mu_assert_int_eq(expected_bsr, bsr);
-    if (shape->arg)
-        free(shape->arg);
-    if (shape->flags)
-        free(shape->flags);
-    if (shape)
-        free(shape);
-    if (str)
-        free(str);
+    free_shape(&shape, result_str);
 }
 
 MU_TEST_SUITE(passing_a_flag_6_minus_30_dot_20_and_a_string_One_Ring_should_be_3_spaces)
 {
     //CONFIG
-    char            *file               = "./files/string/sixminus30dot20";
-    int             fd = open(file, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
-    if (!fd)
-        return ;
-    t_format        *shape              = (t_format *) malloc(sizeof(t_format));
-    if (!shape)
-        return ;
+    int             fd;
+    char            *file               = "./files/string/6minus30dot20";
+    t_format        *shape              = setup(file, &fd);
+
     //ARRANGE
-    shape->type                         = 's';
-    shape->flags                        = ft_strdup("------30.20");
+    int             bsr;
+    char            *result_str;
     char            *str                = "One Ring";
     char            *expected_result    = "One Ring                      ";
-    int             bsr;
     int             expected_bsr        = 30;
+    shape->type                         = 's';
+    shape->flags                        = ft_strdup("------30.20");
 
     //ACT
     bsr = printstring(fd, str, &shape);
-    close(fd);
-    fd = open(file, O_RDONLY);
-    str = get_next_line(fd);
-    remove(file);
+    result_str = unset(fd, file);
 
     //ASSERTS
-    mu_assert_string_eq(expected_result, str);
+    mu_assert_string_eq(expected_result, result_str);
     mu_assert_int_eq(expected_bsr, bsr);
-    if (shape->arg)
-        free(shape->arg);
-    if (shape->flags)
-        free(shape->flags);
-    if (shape)
-        free(shape);
-    if (str)
-        free(str);
+    free_shape(&shape, result_str);
 }
 
 MU_TEST_SUITE(passing_a_flag_1_and_a_empty_string_should_be_space)
 {
     //CONFIG
+    int             fd;
     char            *file               = "./files/string/space1";
-    int             fd = open(file, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
-    if (!fd)
-        return ;
-    t_format        *shape              = (t_format *) malloc(sizeof(t_format));
-    if (!shape)
-        return ;
+    t_format        *shape              = setup(file, &fd);
+
     //ARRANGE
-    shape->type                         = 's';
-    shape->flags                        = ft_strdup("1");
+    int             bsr;
+    char            *result_str;
     char            *str                = "";
     char            *expected_result    = " ";
-    int             bsr;
     int             expected_bsr        = 1;
+    shape->type                         = 's';
+    shape->flags                        = ft_strdup("1");
 
     //ACT
     bsr = printstring(fd, str, &shape);
-    close(fd);
-    fd = open(file, O_RDONLY);
-    str = get_next_line(fd);
-    remove(file);
+    result_str = unset(fd, file);
 
     //ASSERTS
-    mu_assert_string_eq(expected_result, str);
+    mu_assert_string_eq(expected_result, result_str);
     mu_assert_int_eq(expected_bsr, bsr);
-    if (shape->arg)
-        free(shape->arg);
-    if (shape->flags)
-        free(shape->flags);
-    if (shape)
-        free(shape);
-    if (str)
-        free(str);
+    free_shape(&shape, result_str);
 }
 
 MU_TEST_SUITE(passing_a_flag_1_and_a_NULL_string_should_be_printf_null)
 {
     //CONFIG
+    int             fd;
     char            *file               = "./files/string/null";
-    int             fd = open(file, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
-    if (!fd)
-        return ;
-    t_format        *shape              = (t_format *) malloc(sizeof(t_format));
-    if (!shape)
-        return ;
+    t_format        *shape              = setup(file, &fd);
+
     //ARRANGE
-    shape->type                         = 's';
-    shape->flags                        = ft_strdup("1");
+    int             bsr;
+    char            *result_str;
     char            *str                = NULL;
     char            *expected_result    = "(null)";
-    int             bsr;
     int             expected_bsr        = 6;
+    shape->type                         = 's';
+    shape->flags                        = ft_strdup("1");
 
     //ACT
     bsr = printstring(fd, str, &shape);
-    close(fd);
-    fd = open(file, O_RDONLY);
-    str = get_next_line(fd);
-    remove(file);
+    result_str = unset(fd, file);
 
     //ASSERTS
-    mu_assert_string_eq(expected_result, str);
+    mu_assert_string_eq(expected_result, result_str);
     mu_assert_int_eq(expected_bsr, bsr);
-    if (shape->arg)
-        free(shape->arg);
-    if (shape->flags)
-        free(shape->flags);
-    if (shape)
-        free(shape);
-    if (str)
-        free(str);
+    free_shape(&shape, result_str);
 }
 
 MU_TEST_SUITE(passing_a_flag_dot_2_and_a_string_gol_should_be_print_go)
 {
     //CONFIG
-    char            *file               = "./files/string/null";
-    int             fd = open(file, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
-    if (!fd)
-        return ;
-    t_format        *shape              = (t_format *) malloc(sizeof(t_format));
-    if (!shape)
-        return ;
+    int             fd;
+    char            *file               = "./files/string/printgo";
+    t_format        *shape              = setup(file, &fd);
+
     //ARRANGE
-    shape->type                         = 's';
-    shape->flags                        = ft_strdup(".2");
+    int             bsr;
+    char            *result_str;
     char            *str                = "gol";
     char            *expected_result    = "go";
-    int             bsr;
     int             expected_bsr        = 2;
+    shape->type                         = 's';
+    shape->flags                        = ft_strdup(".2");
 
     //ACT
     bsr = printstring(fd, str, &shape);
-    close(fd);
-    fd = open(file, O_RDONLY);
-    str = get_next_line(fd);
-    remove(file);
+    result_str = unset(fd, file);
 
     //ASSERTS
-    mu_assert_string_eq(expected_result, str);
+    mu_assert_string_eq(expected_result, result_str);
     mu_assert_int_eq(expected_bsr, bsr);
-    if (shape->arg)
-        free(shape->arg);
-    if (shape->flags)
-        free(shape->flags);
-    if (shape)
-        free(shape);
-    if (str)
-        free(str);
+    free_shape(&shape, result_str);
 }
 
 MU_TEST_SUITE(test_suite)
