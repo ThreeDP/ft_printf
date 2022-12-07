53a7215e315d3e9296480f93f54fba6daaf728c8
Threedp <davy.paulino@aluno.ifsp.edu.br>
2022-12-07 08:53:27 -0300

    testando refactoring

--- minunit/ft_printf_test.h
+++ minunit/ft_printf_test.h
@@ -8,7 +8,4 @@
 # include <fcntl.h>
 # include <limits.h>
 
-t_format    *setup(char *file, int  *fd);
-char        *unset(int fd, char *file);
-void        free_shape(t_format **shape, char *str);
 #endif
